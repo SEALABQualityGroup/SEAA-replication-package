2d13f1c44709d478d482029e7fd367247c09eadc
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2021-04-07 13:15:33 +0200

    add results and case-study

---
+++ data/solution_with_pas_095/Exp/R/IGD+.Boxplot.R
@@ -0,0 +1,17 @@
+postscript("IGD+.Boxplot.eps", horizontal=FALSE, onefile=FALSE, height=8, width=12, pointsize=10)
+resultDirectory<-"../data"
+qIndicator <- function(indicator, problem)
+{
+fileNSGAII<-paste(resultDirectory, "NSGAII", sep="/")
+fileNSGAII<-paste(fileNSGAII, problem, sep="/")
+fileNSGAII<-paste(fileNSGAII, indicator, sep="/")
+NSGAII<-scan(fileNSGAII)
+
+algs<-c("NSGAII")
+boxplot(NSGAII,names=algs, notch = FALSE)
+titulo <-paste(indicator, problem, sep=":")
+title(main=titulo)
+}
+par(mfrow=c(3,3))
+indicator<-"IGD+"
+qIndicator(indicator, "train-ticket_Length_4_CloningWeight_1.5_MaxCloning_3_MaxEval_82")
